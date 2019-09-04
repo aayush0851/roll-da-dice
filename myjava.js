d7649d733793c6d94c40d2794b9e09eaf4fb4c23
aayush0851 <tyagiayush851@gmail.com>
2019-09-04 19:19:21 +0530

    made the dice roll

--- myjava.js
+++ myjava.js
@@ -1,8 +1,16 @@
 var y = 0;
+let z = 20;
 function fxn1() {
     "use strict";
     var x = Math.floor(Math.random() * 6) + 1;
     y=y+x;
     document.getElementById('score').innerHTML = y;
     document.getElementById('img').src="dice-"+x+".png";
+
 }
+
+
+   
+
+
+            
