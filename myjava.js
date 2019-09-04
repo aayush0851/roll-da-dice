bfb0c97265d48fbdf8cb897a2276e2774374480c
aayush0851 <tyagiayush851@gmail.com>
2019-09-04 11:45:47 +0530

    first commit

---
+++ myjava.js
@@ -0,0 +1,8 @@
+var y = 0;
+function fxn1() {
+    "use strict";
+    var x = Math.floor(Math.random() * 6) + 1;
+    y=y+x;
+    document.getElementById('score').innerHTML = y;
+    document.getElementById('img').src="dice-"+x+".png";
+}
