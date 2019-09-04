5b82cf1626f1206ad20bb5d0d712a8cd1d9c9248
aayush0851 <tyagiayush851@gmail.com>
2019-09-04 22:23:20 +0530

    timer-installed

--- myjava.js
+++ myjava.js
@@ -1,16 +1,31 @@
 var y = 0;
-let z = 20;
+let t = 20;
 function fxn1() {
     "use strict";
     var x = Math.floor(Math.random() * 6) + 1;
     y=y+x;
     document.getElementById('score').innerHTML = y;
     document.getElementById('img').src="dice-"+x+".png";
-
+    if (t==20) {
+        cntdn();
+    }
+    else if(t<0){
+        document.getElementById("rollButton").disabled=true;
+    }
+}
+function cntdn() {
+    if (t<0) {
+        document.getElementById("rollButton").disabled=true;
+    }
+    else {
+        document.getElementById("timer").innerHTML=t+"s";
+        --t;
+    }
+    setTimeout(function(){cntdn();},1000);
 }
 
 
-   
+
 
 
             
